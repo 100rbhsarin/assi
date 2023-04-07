c6c741101c2df92f56a2f5676b7edf6f790e0349
100rbhsarin <saurabh007630@gmail.com>
2023-04-07 11:47:13 +0530

    "#" updated

--- main.js
+++ main.js
@@ -5,29 +5,96 @@ function color(z) {
     }
 
 
-
     
+  
+const gamebox = document.getElementById('gamebox')
+const SQUARES = 209
+const squareColor = "none"
 
-// create color buttons
-const colorButtonContainer = document.getElementById('color-buttons');
-colorOptions.forEach((colorOption) => {
-  const colorButton = document.createElement('button');
-  colorButton.style.backgroundColor = colorOption;
-  colorButton.addEventListener('click', () => {
-    selectedColor = colorOption; // set selected color
-    updateSelectedColor(); // update display of selected color
-  });
-  colorButtonContainer.appendChild(colorButton);
+for(let i= 0; i < SQUARES; i++){
+  const square = document.createElement('div')
+  square.classList.add('square')
+
+  square.addEventListener('mouseover', () => removeColor(square))
+
+  gamebox.appendChild(square)
+}
+
+function removeColor(element){
+  element.style.background = squareColor
+  element.style.boxShadow = `none`
+}
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const okButton = document.getElementById('ok-button');
+let ball = {x: 0, y: 0};
+
+let isDrawing = false;
+let path = [];
+
+canvas.addEventListener('mousedown', function(event) {
+  isDrawing = true;
+  path.push({x: event.clientX, y: event.clientY});
 });
 
-// function to update display of selected color
-function updateSelectedColor() {
-  const colorButtons = document.querySelectorAll('#color-buttons button');
-  colorButtons.forEach((colorButton) => {
-    if (colorButton.style.backgroundColor === selectedColor) {
-      colorButton.classList.add('selected');
-    } else {
-      colorButton.classList.remove('selected');
+canvas.addEventListener('mousemove', function(event) {
+  if (isDrawing) {
+    path.push({x: event.clientX, y: event.clientY});
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.moveTo(path[0].x, path[0].y);
+
+    for (let i = 1; i < path.length; i++) {
+      ctx.lineTo(path[i].x, path[i].y);
     }
-  });
+
+    ctx.stroke();
+  }
+});
+
+canvas.addEventListener('mouseup', function(event) {
+  isDrawing = false;
+});
+
+okButton.addEventListener('click', function() {
+  animateBall();
+});
+
+function animateBall() {
+  let i = 0;
+
+  function moveBall() {
+    if (i < path.length) {
+      ball.x = path[i].x;
+      ball.y = path[i].y;
+      i++;
+      drawBall();
+     
+      requestAnimationFrame(moveBall);
+    }
+  }
+
+  moveBall();
 }
+
+
+
+function selectBall(ball) {
+ 
+const balls = document.querySelectorAll('.ball');
+for (let i = 0; i < balls.length; i++) {
+  balls[i].addEventListener('click', () => selectBall(balls[i]));
+}
+}
+
+function drawBall() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, 10, 0, Math.PI * 2);
+  ctx.fillStyle = 'pink';
+  ctx.fill();
+}
+
+
